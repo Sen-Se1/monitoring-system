42b41c0a1a6f057b51c0b7ec0be253389e81a5b4
SenSe1 <102629374+Sen-Se1@users.noreply.github.com>
2025-11-06 16:56:01 +0100

    Remove monitoring configuration file

--- config.py
+++
@@ -1,8 +0,0 @@
-# Configuration de surveillance
-MONITORING_INTERVAL = 60  # secondes
-CPU_THRESHOLD = 80.0
-MEMORY_THRESHOLD = 85.0
-DISK_THRESHOLD = 90.0
-
-# Services à surveiller
-MONITORED_SERVICES = ["nginx", "mysql", "ssh", "docker", "apache2"]
